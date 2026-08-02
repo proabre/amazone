e0cf676da01dfa1ba3a21a2eb44f3dd0068a2d32
proabre <proabresh@gmail.com>
2026-08-02 22:44:39 +0300

    put products in array of object in amazon.js

---
+++ amazon.js
@@ -0,0 +1,12 @@
+//putting products in an array of objects
+const product=[{
+    image :"images/products/athletic-cotton-socks-6-pairs.jpg",
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating:{
+        stars: 4.5,
+        count: 1234
+    },
+    price: 9.99,
+
+}
+]
