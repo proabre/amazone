91a931d29ec6843b1d20e91bd7d9295fe70f052d
proabre <proabresh@gmail.com>
2026-08-04 12:44:07 +0300

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 2,
+    },
+  ];
+}
+
+function saveToCartStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 export function addToCart(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
@@ -23,6 +31,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToCartStorage();
 }
 
 function removeFromCart(productId) {
@@ -34,6 +43,7 @@ function removeFromCart(productId) {
     }
 
     cart = newCart;
+    saveToCartStorage();
   });
 }
 
